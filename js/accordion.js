d82445377efee2ccb3c51a0d01bbf6f5846f9b64
jeonlegend97 <110451310+jeonlegend97@users.noreply.github.com>
2024-02-15 14:41:54 +0900

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+/*$(document).ready(function() {
+	$( "#accordion" ).accordion({
+		active: false,
+		collapsible: true,
+		heightStyle: "content" 
+	});*/
+	
+	/*$("#m_btn").click(function() {
+		$("#accordion").animate({"left" : "0px"})	
+		$("#cover").css({"display" : "block"})
+		$("#wrap").css({"height" : "800px", "overflow" : "hidden"})
+	});
+	
+	$("#cover").click(function() {
+		$("#accordion").animate({"left" : "-200px"})	
+		$("#cover").css({"display" : "none"})
+		$("#wrap").css({"height" : "auto", "overflow" : "auto"})
+	});*/
+	
+	/*$(function() {
+		var state= true;
+		
+		$("#nav_menu_btn").on("click", function() {
+			if (state){
+				$("#accordion").animate({"right" : "0"});
+				$("#cover").css({"display" : "block"});
+				$("#wrap").animate({"right":"400px"});
+				$("#wrap").css({"position" : "fixed", "overflow" : "hidden"});
+			} else {
+				$("#accordion").animate({"right" : "-400px"});
+				$("#cover").css({"display" : "none"});
+				$("#wrap").animate({"right":"0"});
+				$("#wrap").css({"position" : "static", "overflow" : "auto"});
+			}
+			state = !state;
+		});
+	});
+});*/
